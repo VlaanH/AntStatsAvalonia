ea1360a37135ba75c747425bbdb5d80b5f51f25d
vlanh <vlan.h@yandex.ua>
2021-08-21 16:46:15 +0300

    Core update (Added method for deleting profiles)

--- Core/Settings/Settings.cs
+++ Core/Settings/Settings.cs
@@ -1,12 +1,15 @@
 using System;
+using System.Collections.Generic;
 using System.IO;
 using System.Text.Json;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 
 namespace AntStatsCore
 {
     public class SettingsData
     {
+        public string NameProfile { get; set; }
         public string Pass { get; set; }
            
         public string User { get; set; }
@@ -41,13 +44,61 @@ namespace AntStatsCore
     public static class Settings
     {
 
-        public static async Task<SettingsData> Get(string path=default)
+        public static async Task<List<SettingsData>> GetProfiles()
         {
+            
+            string profilesDirectory="ProfilesAntStats";
+            
+            List<SettingsData> settingsDatas=new List<SettingsData>();
+            
+            List<string> directorys= new List<string>();
+            
+            await Task.Run(() =>
+            {
+               var fdirectorys= new DirectoryInfo(profilesDirectory).GetFiles();
+
+               for (int i = 0; i < fdirectorys.Length; i++)
+               {
+                   var pattern = @"([\w \W ]+).json";
+                   string noExtension = Regex.Match(fdirectorys[i].Name, pattern).Groups[1].Value;
+                   if (noExtension!="")
+                       directorys.Add(noExtension);  
+                   
+               }
+               
+            });
+            
+            
+            for (int i = 0; i < directorys.Count; i++)
+            {
+               var settings = await Get(default, profilesDirectory + "/" + directorys[i]);
+               
+               settings.NameProfile = directorys[i];
+               
+               settingsDatas.Add(settings);
+               
+            }
+           
+            
+            
+            
+            return settingsDatas;
+        }
+
+        
+
+
+        public static async Task<SettingsData> Get(string profilename,string path)
+        {
+            
+            string profilesDirectory="ProfilesAntStats";
+
+
             SettingsData settings = new SettingsData();
                 
                 try
                 {
-                    using (FileStream fs = new FileStream(path+"settings.json", FileMode.OpenOrCreate))
+                    using (FileStream fs = new FileStream(path+profilesDirectory+"/"+profilename+".json", FileMode.OpenOrCreate))
                     {
                         settings = await JsonSerializer.DeserializeAsync<SettingsData>(fs);
                     }
@@ -61,12 +112,25 @@ namespace AntStatsCore
           
         }
 
-
+        public static async void DeleteSettingsProfile(string profilename,string path)
+        {
+            string profilesDirectory="ProfilesAntStats";
+            await Task.Run(() =>
+            {
+                if (File.Exists(path+profilesDirectory+"/"+profilename+".json"))
+                    File.Delete(path+profilesDirectory+"/"+profilename+".json");
+                
+            });
+        }
 
         public static async void Save(SettingsData settingsClass,string path=default)
         {
-           
-            using (FileStream fs = new FileStream(path+"settings.json", FileMode.OpenOrCreate))
+            string profilesDirectory="ProfilesAntStats";
+            if (Directory.Exists(profilesDirectory) == false)
+                Directory.CreateDirectory(profilesDirectory);
+            
+            
+            using (FileStream fs = new FileStream(path+profilesDirectory+"/"+settingsClass.NameProfile+".json", FileMode.OpenOrCreate))
             { 
                 //cleaning
                 fs.SetLength(default);
